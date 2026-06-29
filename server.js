17c4fadcc38999507fef710308ed53984c4933e0
AMN-TUF <yo.akaman@gmail.com>
2026-06-29 20:24:05 +0530

    Update

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ require("dotenv").config();
 
 const app = require("./src/app")
 const connectDB = require('./src/db/db')
-const userController = require("./src/controller/user.controller")
 
 connectDB();
 
 const PORT = process.env.PORT || 5000
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Resume AI Backend is running"
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
 
-app.get('/', (req, res) => {
-    res.send("Home page")
-})
 
 module.exports = app;
