6dcf6b6bb872d23dcc51dda63b55a768e2a5ce6a
AMN-TUF <yo.akaman@gmail.com>
2026-07-01 17:20:30 +0530

    Remove unwanted cors

--- src/app.js
+++ src/app.js
@@ -8,10 +8,7 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json())
 app.use(cookieParser());
